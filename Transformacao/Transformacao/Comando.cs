fb8c34d085393dd3adfc57d4e9efa2409b1aa2c4
Eduarda Helena <dudinhahelenaa@gmail.com>
2016-07-15 10:52:39 -0300

    Versao 1

---
+++ Transformacao/Transformacao/Comando.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformacao
+{
+    class Comando
+    {
+        static void Main(string[] args)
+        {
+           Contas p = new Contas();
+
+            Console.WriteLine("Qual número voce deseja converter?");
+            try
+            {
+               
+                p.limit = int.Parse(Console.ReadLine()); 
+                if(p.limit < 0)
+                { Console.WriteLine("Não é possível converter números negativos");
+                p.limit = int.Parse("dasd"); 
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERR0!!!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            p.DecimalToBinario(p.limit);
+          //  Console.WriteLine(divisao);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Transformacao/Transformacao/Contas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformacao
+{
+    class Contas
+    {
+       public int limit = 100;
+       public int result;
+       List<int> Restos = new List<int>();
+       int controle = 0;
+       string Final = "";
+    
+        public void DecimalToBinario(int limit)
+        {
+                controle = 0; 
+                result = limit;
+
+                while (result > 1)
+                {
+                    Restos.Add(result % 2);
+                    result = result/2;
+                    Final+=Restos[controle].ToString();
+                    controle++;
+                }
+                Restos.Add(result);
+                Final+=Restos[controle].ToString();
+                Console.Write (Reverse( Final));     
+                Console.ReadKey();
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+       
+
+
+    }
+}
